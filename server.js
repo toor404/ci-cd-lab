f64d9130c797cdabf18d38baf1d26d493eceb900
root <farizsetara@gmail.com>
2023-03-30 02:16:02 +0000

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const {greeting} = require('./user')
 
-app.get('/', (req,res) => res.send('Hello Netters!'))
+app.get('/', (req,res) => res.send('Hallo Netters!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
